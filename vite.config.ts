9ab4deb7c5415033ef70bccfd53797fab7e8d452
Julien Jean <julien.jean@platform.sh>
2024-02-08 15:03:38 +0100

    parametize port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = process.env.PORT ? parseInt(process.env.PORT) : 5173
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port,
+    host: '0.0.0.0',
+  }
 })
